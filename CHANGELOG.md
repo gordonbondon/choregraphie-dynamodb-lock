e9370a7792bab7b48b7834aa4dd4458bea1d3620
Artem Iarmoliuk <koolgen@gmail.com>
2018-10-15 13:39:35 +0300

    Release 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of the Choregraphie DynamoDB Lock cookbook.
 
+## 0.2.0 (2018-10-15)
+
+- Add support for expires attribute name
+
 ## 0.1.2 (2018-07-23)
 
 - Fix unit and lint tests
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'artem.yarmoluk@gmail.com'
 license 'MIT'
 description 'AWS DynamoDB lock primitive for chef choregraphie'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.2.0'
 issues_url 'https://github.com/gordonbondon/choregraphie-dynamodb-lock/issues'
 source_url 'https://github.com/gordonbondon/choregraphie-dynamodb-lock'
 chef_version '>= 12.6' if respond_to?(:chef_version)
